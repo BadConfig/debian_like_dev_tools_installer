791ae52ee32003e7fdfe1c87a55125b458f34568
ahmet <exit.lab@mail.ru>
2019-08-17 17:48:50 +0300

    fix

--- cge.sh
+++ cge.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#to get root 
-su
-
 #for fresh repos
-apt-get -y update && apt-get -y  upgrade
+sudo apt-get -y update && sudo apt-get -y  upgrade
 
 #installing main pakages
-apt-get install -y sudo nasm gcc vim git screenfetch build-essential cmake
+sudo apt-get install -y sudo nasm gcc vim git screenfetch build-essential cmake
 python3-dev python3  
 #screenfetch
-echo "\n#screenfetch autorun\nif [ -f /usr/bin/screenfetch ]; then screenfetch;fi"
+echo "\n#screenfetch autorun\nif [ -f /usr/bin/screenfetch ]; then
+screenfetch;fi" >> ~/.bashrc
 
 #setup aliases
 cp ./.bash_aliases ~/.bash_aliases 
@@ -20,8 +18,6 @@ cp ./.bash_aliases ~/.bash_aliases
 cp ./.vimrc ~/.vimrc -r 
 apt-get install clang-tools-8
 update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-8 100
-cp ./.vim ~/.vim 
-~/.vim/bundle/YouCompleteMe/install.py --all
-~/.vim/bundle/YouCompleteMe/install.py --clangd-completer
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
